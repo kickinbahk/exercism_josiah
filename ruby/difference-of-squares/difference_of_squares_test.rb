d1c2f53c7d20a907033ca5da6dde5a817b90da54
kickinbahk <kickinbahk@gmail.com>
2015-11-12 09:30:35 -0800

    fixed test file for git issue

--- ruby/difference-of-squares/difference_of_squares_test.rb
+++ ruby/difference-of-squares/difference_of_squares_test.rb
@@ -25,60 +25,32 @@ class DifferenceOfSquaresTest < Minitest::Test
   end
 
   def test_sum_of_squares_to_10
-<<<<<<< HEAD
-=======
-    skip
->>>>>>> 3c3a3f3363e84c35295b86d9c7384859d72de8d8
     assert_equal 385, Squares.new(10).sum_of_squares
   end
 
   def test_difference_of_sums_to_10
-<<<<<<< HEAD
-=======
-    skip
->>>>>>> 3c3a3f3363e84c35295b86d9c7384859d72de8d8
     assert_equal 2_640, Squares.new(10).difference
   end
 
   def test_square_of_sums_to_100
-<<<<<<< HEAD
-=======
-    skip
->>>>>>> 3c3a3f3363e84c35295b86d9c7384859d72de8d8
     assert_equal 25_502_500, Squares.new(100).square_of_sums
   end
 
   def test_sum_of_squares_to_100
-<<<<<<< HEAD
-=======
-    skip
->>>>>>> 3c3a3f3363e84c35295b86d9c7384859d72de8d8
     assert_equal 338_350, Squares.new(100).sum_of_squares
   end
 
   def test_difference_of_sums_to_100
-<<<<<<< HEAD
-=======
-    skip
->>>>>>> 3c3a3f3363e84c35295b86d9c7384859d72de8d8
     assert_equal 25_164_150, Squares.new(100).difference
   end
 
   def test_consistent_difference
-<<<<<<< HEAD
-=======
-    skip
->>>>>>> 3c3a3f3363e84c35295b86d9c7384859d72de8d8
     squares = Squares.new(10)
     assert_equal squares.difference, squares.difference
   end
 
   # Don't foget to define a constant VERSION inside of Squares.
   def test_bookkeeping
-<<<<<<< HEAD
-=======
-    skip
->>>>>>> 3c3a3f3363e84c35295b86d9c7384859d72de8d8
     assert_equal 1, Squares::VERSION
   end
 end
