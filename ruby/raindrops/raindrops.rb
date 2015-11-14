2609a5d6cc998e4ae3be374b8a59b52e350e2340
kickinbahk <kickinbahk@gmail.com>
2015-11-13 16:28:59 -0800

    Refactor raindrops to be clearer, chain methods and have less fewer variables.

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,22 +2,19 @@ class Raindrops
   require 'prime'
   
   def self.convert(number)
-   
-    @number_array = number.prime_division
     factored_array = []
 
-    @number_array.map do |factor|
-
+    number.prime_division.map do |factor|
       case
       when factor.include?(3)
-        drop = "Pling"
+        factored_array.push("Pling")
       when factor.include?(5)
-        drop = "Plang"
+        factored_array.push("Plang")
       when factor.include?(7)
-        drop = "Plong"
+        factored_array.push("Plong")
       end
-      factored_array.push(drop)      
     end
+    
     if factored_array.any? == false
       number.to_s
     else
