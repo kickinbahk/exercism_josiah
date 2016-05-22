f804c8bd450deeeb06c520125d0f945d90066935
kickinbahk <kickinbahk@gmail.com>
2016-05-21 17:35:15 -0700

    move if statement to own function

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,11 +1,23 @@
 var DnaTranscriber = function () {}
 
 DnaTranscriber.prototype.toRna = function (input) {
-  if (input === 'C') {
-    return 'G'
-  } else if (input === 'G') {
-    return 'C'
+  return compare(input).join('')
+}
+
+function compare (dnaString) {
+  var dnaArray = dnaString.split('')
+  var rnaArray = []
+
+  for (var i = 0; i < dnaArray.length; i++) {
+    if (dnaArray[i] === 'C') {
+      rnaArray.push('G')
+    } else if (dnaArray[i] === 'G') {
+      rnaArray.push('C')
+    } else if (dnaArray[i] === 'A') {
+      rnaArray.push('U')
+    }
   }
+  return rnaArray
 }
 
 module.exports = DnaTranscriber
