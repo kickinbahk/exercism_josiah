50aaf4e59b5e747a405e293407ce3783878c5b82
kickinbahk <kickinbahk@gmail.com>
2016-05-21 17:18:09 -0700

    add if to catch second possiblilty

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,7 +1,11 @@
 var DnaTranscriber = function () {}
 
 DnaTranscriber.prototype.toRna = function (input) {
-  return 'G'
+  if (input === 'C') {
+    return 'G'
+  } else if (input === 'G') {
+    return 'C'
+  }
 }
 
 module.exports = DnaTranscriber
