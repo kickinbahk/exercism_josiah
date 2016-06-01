6641b0508409f39c882519901d735f24284712a2
kickinbahk <kickinbahk@gmail.com>
2016-06-01 11:45:27 -0700

    add function to pass first test.

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,3 +1,10 @@
-var Gigasecond = function () {}
+var Gigasecond = function (givenDate) {
+  this.newDate = givenDate
+}
+
+Gigasecond.prototype.date = function () {
+  var convertedToMilliseconds = this.newDate.getTime()
+  return new Date(convertedToMilliseconds + 1000000000000)
+}
 
 module.exports = Gigasecond
